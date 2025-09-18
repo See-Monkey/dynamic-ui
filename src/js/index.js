8b38fdee86010a1964ddb3a2026d31206e75d143
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2025-09-17 22:31:40 -0600

    Add header dropdowns

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
 console.log("Hi, Mom!");
 
 import "../css/style.css";
+
+const linksBtn = document.querySelector("#linksBtn");
+const hamburgerBtn = document.querySelector("#hamburgerBtn");
+
+const linkContainer = document.querySelector(".linkContainer");
+const nav = document.querySelector(".nav");
+
+linksBtn.addEventListener("click", function() {
+    linkContainer.classList.toggle("active");        
+});
+
+hamburgerBtn.addEventListener("click", function() {
+    nav.classList.toggle("active");          
+});
