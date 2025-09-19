cd5852e1edef7f1bf56b625871a32b7bf780d3e9
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2025-09-18 20:30:14 -0600

    Add rotating image carousel

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # dynamic-ui
 The Odin Project - Dynamic User Interface Interactions
+
+Image credits belong to:
+
+Christopher Kuzman
+https://unsplash.com/photos/a-couple-of-water-towers-sitting-in-the-middle-of-a-desert-PM1E_M4zTrM?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash
+
+Luke Vodell
+https://unsplash.com/photos/brown-and-green-camouflage-tent-speT7Iohgps?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash
+
+Maksim Istomin
+https://unsplash.com/photos/person-in-brown-and-black-camouflage-jacket-wearing-white-helmet-standing-on-brown-tree-branch-during-BSx5n20J-qg?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash
+
+Matthieu Bühler
+https://unsplash.com/photos/river-surrounded-with-high-rise-building-during-nighttime-WnfKYqxWH8Q?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ console.log("Hi, Mom!");
 
 import "../css/style.css";
 
+// ========== dropdown menus ========== //
 const linksBtn = document.querySelector("#linksBtn");
 const hamburgerBtn = document.querySelector("#hamburgerBtn");
 
@@ -15,3 +16,41 @@ linksBtn.addEventListener("click", function() {
 hamburgerBtn.addEventListener("click", function() {
     nav.classList.toggle("active");          
 });
+
+// ========== image carousel ========== //
+
+const slides = document.querySelectorAll(".slide");
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+let slideIndex = 0;
+let intervalID = null;
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    
+    slides.forEach(slide => {
+        slide.classList.remove("activeSlide");
+    });
+    slides[slideIndex].classList.add("activeSlide");
+    intervalID = setInterval(nextSlide, 5000)
+}
+
+function prevSlide() {
+    clearInterval(intervalID);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide() {
+    clearInterval(intervalID);
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", nextSlide);
+document.addEventListener("DOMContentLoaded", showSlide(slideIndex));
